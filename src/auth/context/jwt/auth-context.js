afb50b3a0e176e33e1c66b4f044952ba0aa7e284
ineinfotechservices <nikhil.ineinfotech@gmail.com>
2024-12-30 18:12:07 +0530

    update useContext

--- src/auth/context/jwt/auth-context.js
+++ src/auth/context/jwt/auth-context.js
@@ -4,4 +4,14 @@ import { createContext } from 'react';
 
 // ----------------------------------------------------------------------
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext({
+    user: null,
+    loginData: [],
+    method: 'jwt',
+    loading: false,
+    authenticated: false,
+    unauthenticated: true,
+    login: () => { },
+    register: () => { },
+    logout: () => { },
+});
